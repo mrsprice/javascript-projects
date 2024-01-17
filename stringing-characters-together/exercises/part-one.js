ac3aaf3c1525314cf7a3b8b812d8374f08907fda
Margaret <simanowitz93@gmail.com>
2024-01-16 21:19:15 -0600

    string part two

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -3,6 +3,12 @@ let num = 1001;
 //Returns 'undefined'.
 console.log(num.length);
 
+if (String(num).includes('.')){
+    console.log(String(num).length-1);
+    } else {
+    console.log(String(num).length);
+    }
+
 //Use type conversion to print the length (number of digits) of an integer.
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
