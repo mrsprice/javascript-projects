93d9ee6214738b6352a5395f7d6b98fbbd760ff3
Margaret <simanowitz93@gmail.com>
2024-02-07 20:52:27 -0600

    Unit testing

---
+++ unit-testing/exercises/tests/RPS.test.js
@@ -0,0 +1,13 @@
+const whoWon = require("../RPS.js");
+
+describe("RPS", function () {
+  test("returns 'Player 2 wins!' if P1 = rock & P2 = paper", function () {
+    let output = test.whoWon("rock", "paper");
+    expect(output).toBe("Player 2 wins!");
+  });
+
+  test("returns 'Player 2 wins!' if P1 = paper & P2 = scissors", function () {
+    let output = test.whoWon("paper", "scissors");
+    expect(output).toBe("Player 2 wins!");
+  });
+});
---
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -0,0 +1,8 @@
+const checkFive = require("../checkFive.js");
+
+describe("checkFive", function () {
+  test("Descriptive feedback...", function () {
+    let output = checkFive(2);
+    expect(output).toEqual("2 is less than 5.");
+  });
+});
